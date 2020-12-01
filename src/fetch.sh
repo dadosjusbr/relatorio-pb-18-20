4a218e6bf6541edc2cef6f8f36044a77f3d9042c
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2020-12-01 11:10:36 -0300

    Adiciona trt13 e tjpb no fetch

--- src/fetch.sh
+++ src/fetch.sh
@@ -3,10 +3,17 @@ set -euo pipefail
 
 cd dados/raw
 
+BASE_URL="https://cloud5.lsd.ufcg.edu.br:8080/swift/v1/dadosjusbr"
+
 anos=`seq 2018 2020`
+orgaos="mppb trt13 tjpb"
 for ano in $anos; do
     for mes in `seq 1 12`; do
-        curl -O https://cloud5.lsd.ufcg.edu.br:8080/swift/v1/dadosjusbr/mppb/mppb-$ano-$mes.zip
+        for orgao in $orgaos; do
+          echo "$orgao $ano-$mes"
+          target=$orgao-$ano-$mes.zip
+          curl -sSO $BASE_URL/$orgao/$target
+        done
     done
 done
 
